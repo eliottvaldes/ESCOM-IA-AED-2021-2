6ff6069c0c3de1fd4c117e990f45550e417d03da
Eliot Valdes <eliottvaldes@hotmail.com>
2022-03-02 06:03:01 -0600

    mod(e): eliminacion de peticion de ejercicio en menu

--- ELIOT/ordenamiento.c
+++ ELIOT/ordenamiento.c
@@ -27,33 +27,13 @@ int loading()
     puts("\n");
     return 0;
 }
-// ask for confirmation
-bool asking(char askForRepetition, bool repeatExercise)
-{
-    if (askForRepetition == 'y')
-    {
-        repeatExercise = true;
-    }
-    else if (askForRepetition == 'n')
-    {
-        repeatExercise = false;
-    }
-    else
-    {
-        printf("\nOpcion no valida, regresando a menu principal ");
-        loading();
-        puts("\n\n");
-        repeatExercise = false;
-    }
-    return repeatExercise;
-}
 
 // CREAMOS PROTOTIPOS DE FUNCIONES
-int * generarArrayNumerosAleatorios(int, int);
+int *generarArrayNumerosAleatorios(int, int);
 void mostrarDatosEnArreglo(int *, int, int);
 
 // create random data in array
-int * generarArrayNumerosAleatorios(int extensionDeArreglo, int rangoDeArreglo)
+int *generarArrayNumerosAleatorios(int extensionDeArreglo, int rangoDeArreglo)
 {
 
     int *arrayNumerico = malloc(extensionDeArreglo * sizeof(int));
@@ -77,7 +57,7 @@ void mostrarDatosEnArreglo(int *arrayNumerico, int extensionDeArreglo, int i)
     else
     {
         printf("%d, ", arrayNumerico[i]);
-    }    
+    }
 }
 
 /*
@@ -160,232 +140,198 @@ void main()
         switch (exerciseSelection)
         {
         case 1:
-            while (repeatExercise == true)
+
+            puts("\nHas seleccionado la opcion de generar array de numeros aleatorios\n");
+            // Start Coding
+
+            // liberamos memoria del arreglo dinamico usando free
+            free(arrayNumerico);
+
+            // pedimos la cantidad de numeros en el array
+            puts("Digita la cantidad de numeros que quieres en el array");
+            fflush(stdin);
+            scanf("%d", &extensionDeArreglo);
+
+            // pedimos el rango para la generacion de numeros aleatorios
+            puts("Digita el rango superior para los numeros del arreglo");
+            fflush(stdin);
+            scanf("%d", &rangoDeArreglo);
+
+            // creamos semilla para generacion de numeros random
+            // usamos srand(time()); para no obtener los mismos numeros aleatorios en cada ejecucion ya que la semilla es la hora actual
+            srand(time(NULL));
+
+            // mandamos a llamar a la funcion de crear numeros de datos aleatorios
+            // arrayNumerico = generarArrayNumerosAleatorios(extensionDeArreglo, rangoDeArreglo);
+            arrayNumerico = malloc(extensionDeArreglo * sizeof(int));
+
+            for (int i = 0; i <= extensionDeArreglo; i++)
             {
-                puts("\nHas seleccionado la opcion de generar array de numeros aleatorios\n");
-                // Start Coding
-
-                // liberamos memoria del arreglo dinamico usando free
-                free(arrayNumerico);
-
-                // pedimos la cantidad de numeros en el array
-                puts("Digita la cantidad de numeros que quieres en el array");
-                fflush(stdin);
-                scanf("%d", &extensionDeArreglo);
-
-                // pedimos el rango para la generacion de numeros aleatorios
-                puts("Digita el rango superior para los numeros del arreglo");
-                fflush(stdin);
-                scanf("%d", &rangoDeArreglo);
-
-                // creamos semilla para generacion de numeros random
-                // usamos srand(time()); para no obtener los mismos numeros aleatorios en cada ejecucion ya que la semilla es la hora actual
-                srand(time(NULL));
-
-                // mandamos a llamar a la funcion de crear numeros de datos aleatorios
-                // arrayNumerico = generarArrayNumerosAleatorios(extensionDeArreglo, rangoDeArreglo);
-                arrayNumerico = malloc(extensionDeArreglo * sizeof(int));
-
-                for (int i = 0; i <= extensionDeArreglo; i++)
-                {
-                    // creamos numeros aleatorios y los vamos agregando al array
-                    arrayNumerico[i] = rand() % rangoDeArreglo;
-                }
-
-                // mostramos mensaje de exito
-                puts("\n*** Arreglo generado exitosamente ***\n");
-
-                // End of Code
-
-                // Ask for confirmation
-                repeatExercise = false;
+                // creamos numeros aleatorios y los vamos agregando al array
+                arrayNumerico[i] = rand() % rangoDeArreglo;
             }
+
+            // mostramos mensaje de exito
+            puts("\n*** Arreglo generado exitosamente ***\n");
+
+            // End of Code
+
             break;
 
         case 2:
-            while (repeatExercise == true)
+
+            puts("\nHas seleccionado la opcion de imprimir datos\n");
+            // Start Coding
+
+            // verificamos que el usuario ya haya creado un arredo de datos numericos aleatorios
+            if (extensionDeArreglo > 0)
             {
-                puts("\nHas seleccionado la opcion de imprimir datos\n");
-                // Start Coding
-
-                // verificamos que el usuario ya haya creado un arredo de datos numericos aleatorios
-                if (extensionDeArreglo > 0)
+                for (int i = 0; i < extensionDeArreglo; i++)
                 {
-                    for (int i = 0; i < extensionDeArreglo; i++)
-                    {
-                        mostrarDatosEnArreglo(arrayNumerico, extensionDeArreglo, i);
-                    }
+                    mostrarDatosEnArreglo(arrayNumerico, extensionDeArreglo, i);
                 }
-                else
-                {
-                    puts("\n*** Aun no has generado un arreglo de numeros aleatorios *** \n");
-                }                
-
-                // End of Code
-
-                // no preguntamos para repetir el ejercicio
-                repeatExercise = false;
             }
+            else
+            {
+                puts("\n*** Aun no has generado un arreglo de numeros aleatorios *** \n");
+            }
+
+            // End of Code
 
             break;
 
         case 3:
-            while (repeatExercise == true)
+
+            puts("\nHas seleccionado ordenamiento por burbuja\n");
+            // Start Coding
+
+            // mostramos mensaje
+            puts("Iniciando ordenamiento burbuja ");
+            loading();
+
+            // comenzamos el ordenamiento burbuja
+            // recorremos el arreglo
+            for (int i = 0; i < extensionDeArreglo; i++)
             {
-                puts("\nHas seleccionado ordenamiento por burbuja\n");
-                // Start Coding
-
-                // mostramos mensaje
-                puts("Iniciando ordenamiento burbuja ");
-                loading();
-
-                // comenzamos el ordenamiento burbuja
-                // recorremos el arreglo
-                for (int i = 0; i < extensionDeArreglo; i++)
+                // recorremos los elementos x veces en el arreglo - i
+                for (int j = 0; j < (extensionDeArreglo - i); j++)
                 {
-                    // recorremos los elementos x veces en el arreglo - i
-                    for (int j = 0; j < (extensionDeArreglo - i); j++)
+                    // hacemos la comparacion
+                    if (arrayNumerico[j] > arrayNumerico[j + 1])
                     {
-                        // hacemos la comparacion
-                        if (arrayNumerico[j] > arrayNumerico[j + 1])
-                        {
-                            // hacemos el intercambio de valores
-                            temp = arrayNumerico[j];
-                            arrayNumerico[j] = arrayNumerico[j + 1];
-                            arrayNumerico[j + 1] = temp;
-                        }
+                        // hacemos el intercambio de valores
+                        temp = arrayNumerico[j];
+                        arrayNumerico[j] = arrayNumerico[j + 1];
+                        arrayNumerico[j + 1] = temp;
                     }
                 }
-                // fin de ordenamiento burbuja
-
-                puts("*** Ordenamiento completado ***\n");
-
-                // End of Code
-                repeatExercise = false;
             }
+            // fin de ordenamiento burbuja
+
+            puts("*** Ordenamiento completado ***\n");
+
+            // End of Code
 
             break;
 
         case 4:
-            while (repeatExercise == true)
+
+            puts("\nHas seleccionado ordenamiento por insercion\n");
+            // Start Coding
+
+            printf("Iniciando ordenamiento insercion ");
+            loading();
+
+            // iniciamos el ordenamiento por insercion
+            for (int i = 0; i < extensionDeArreglo; i++)
             {
-                puts("\nHas seleccionado ordenamiento por insercion\n");
-                // Start Coding
 
-                printf("Iniciando ordenamiento insercion ");
-                loading();
-
-                // iniciamos el ordenamiento por insercion
-                for (int i = 0; i < extensionDeArreglo; i++)
+                temp = arrayNumerico[i];
+                j = (i - 1);
+                while ((arrayNumerico[j] > temp) && (j >= 0))
                 {
-
-                    temp = arrayNumerico[i];
-                    j = (i - 1);
-                    while ((arrayNumerico[j] > temp) && (j >= 0))
-                    {
-                        arrayNumerico[j + 1] = arrayNumerico[j];
-                        j--;
-                    }
-                    arrayNumerico[j + 1] = temp;
+                    arrayNumerico[j + 1] = arrayNumerico[j];
+                    j--;
                 }
-
-                puts("*** Ordenamiento completado ***\n");
-                // End of Code
-
-                // Ask for confirmation
-                repeatExercise = false;
+                arrayNumerico[j + 1] = temp;
             }
+
+            puts("*** Ordenamiento completado ***\n");
+
+            // End of Code
 
             break;
 
         case 5:
-            while (repeatExercise == true)
+
+            puts("\nHas seleccionado ordenamiento por seleccion\n");
+            // Start Coding
+
+            printf("Iniciando ordenamiento seleccion ");
+            loading();
+
+            // iniciando algoritmo de ordenamiento por seleccion
+            for (int i = 0; i < extensionDeArreglo; i++)
             {
-                puts("\nHas seleccionado ordenamiento por seleccion\n");
-                // Start Coding
-
-                printf("Iniciando ordenamiento seleccion ");
-                loading();
-
-                // iniciando algoritmo de ordenamiento por seleccion
-                for (int i = 0; i < extensionDeArreglo; i++)
+                int pos_menor = i;
+                // buscamos el valor menor
+                for (int j = i + 1; j < extensionDeArreglo; j++)
                 {
-                    int pos_menor = i;
-                    // buscamos el valor menor
-                    for (int j = i + 1; j < extensionDeArreglo; j++)
+                    // comparamos el valor de la pos j  para ver si es menor
+                    if (arrayNumerico[j] < arrayNumerico[pos_menor])
                     {
-                        // comparamos el valor de la pos j  para ver si es menor
-                        if (arrayNumerico[j] < arrayNumerico[pos_menor])
-                        {
-                            // cambiamos la pos_menor por el valor menor que es j
-                            pos_menor = j;
-                        }
+                        // cambiamos la pos_menor por el valor menor que es j
+                        pos_menor = j;
                     }
-                    // realizamos el cambio de valores
-                    temp = arrayNumerico[pos_menor];
-                    arrayNumerico[pos_menor] = arrayNumerico[i];
-                    arrayNumerico[i] = temp;
                 }
-
-                // for (int i = 0; i < (extensionDeArreglo -1); i++)
-                // {
-                //     // obtenemos el dato menor de la lista desde el indice i
-                //     int pos_menor = BuscaMenor(arrayNumerico, extensionDeArreglo, i);
-                //     // intercambiamos datos
-                //     temp = arrayNumerico[i];
-                //     arrayNumerico[i] = pos_menor;
-                //     arrayNumerico[pos_menor] = temp;
-
-                // }
-
-                puts("*** Ordenamiento completado ***\n");
-
-                // End of Code
-
-                // Ask for confirmation
-                repeatExercise = false;
+                // realizamos el cambio de valores
+                temp = arrayNumerico[pos_menor];
+                arrayNumerico[pos_menor] = arrayNumerico[i];
+                arrayNumerico[i] = temp;
             }
+
+            // for (int i = 0; i < (extensionDeArreglo -1); i++)
+            // {
+            //     // obtenemos el dato menor de la lista desde el indice i
+            //     int pos_menor = BuscaMenor(arrayNumerico, extensionDeArreglo, i);
+            //     // intercambiamos datos
+            //     temp = arrayNumerico[i];
+            //     arrayNumerico[i] = pos_menor;
+            //     arrayNumerico[pos_menor] = temp;
+
+            // }
+
+            puts("*** Ordenamiento completado ***\n");
+
+            // End of Code
 
             break;
 
         case 6:
-            while (repeatExercise == true)
-            {
-                puts("\nHas seleccionado ordenamiento por merge\n");
-                // Start Coding
 
-                printf("Iniciando ordenamiento merge ");
-                loading();
+            puts("\nHas seleccionado ordenamiento por merge\n");
+            // Start Coding
 
-                // iniciamos codigo para el algoritmo de Merge
+            printf("Iniciando ordenamiento merge ");
+            loading();
 
-                puts("*** Ordenamiento completado ***\n");
-                // End of Code
+            // iniciamos codigo para el algoritmo de Merge
 
-                // Ask for confirmation
-                repeatExercise = false;
-            }
+            puts("*** Ordenamiento completado ***\n");
+            // End of Code
 
             break;
 
         case 7:
-            while (repeatExercise == true)
-            {
-                puts("\nHas seleccionado ordenamiento por quick sort\n");
-                // Start Coding
 
-                printf("Iniciando ordenamiento burbuja ");
-                loading();
-                puts("*** Ordenamiento completado ***\n");
+            puts("\nHas seleccionado ordenamiento por quick sort\n");
+            // Start Coding
 
-                // End of Code
+            printf("Iniciando ordenamiento burbuja ");
+            loading();
+            puts("*** Ordenamiento completado ***\n");
 
-                // Ask for confirmation
-                puts("Para repetir el ejercicio digita ' y '. Para regresar al menu digita ' n '.");
-                fflush(stdin);
-                scanf("%c", &askForRepetition);
-                repeatExercise = asking(askForRepetition, repeatExercise);
-            }
+            // End of Code
 
             break;
 

22959428e7759447facb98d8768a3d859ce2cc4b
Jose <mnry7999@gmail.com>
2022-03-08 14:59:33 -0600

    add: EstructuraAnidada_Menu.c
    Se añade el ejercico de estructuras anidadas y busqueda, organizadas en un menú

--- MONROY/Busqueda.c
+++ MONROY/Busqueda.c
@@ -39,115 +39,115 @@ void ordenarDatos(int *cadenaNum, int limite)
         cadenaNum[i] = temp;
     }
 }
-    int busquedaSecuencial(int *arreglo, int longitud, int numeroBusqueda)
+int busquedaSecuencial(int *arreglo, int longitud, int numeroBusqueda)
+{
+    for (int i = 0; i < longitud; i++)
     {
-        for (int i = 0; i < longitud; i++)
+        if (arreglo[i] == numeroBusqueda)
         {
-            if (arreglo[i] == numeroBusqueda)
-            {
-                printf("El numero %d esta en la posicion numero %d del arreglo", numeroBusqueda, i + 1);
-                return 1;
-            }
+            printf("El numero %d esta en la posicion numero %d del arreglo", numeroBusqueda, i + 1);
+            return 1;
         }
-        printf("El numero %d no se encuentra el la ultimo arreglo registrado", numeroBusqueda);
-        return 0;
     }
+    printf("El numero %d no se encuentra el la ultimo arreglo registrado", numeroBusqueda);
+    return 0;
+}
 
-    int busquedaBinaria(int *arreglo, int longitud, int numeroBusqueda)
+int busquedaBinaria(int *arreglo, int longitud, int numeroBusqueda)
+{
+    int min = 0;
+    int max = (longitud - 1);
+    while (min <= max)
     {
-        int min = 0;
-        int max = (longitud - 1);
-        while (min <= max)
+        int middle = ((max - min) / 2) + min;
+        if (arreglo[middle] == numeroBusqueda)
         {
-            int middle = ((max - min) / 2) + min;
-            if (arreglo[middle] == numeroBusqueda)
+            printf("El numero %d esta en la posicion numero %d del arreglo", numeroBusqueda, middle + 1);
+            return 1;
+        }
+        else
+        {
+            if (numeroBusqueda < arreglo[middle])
             {
-                printf("El numero %d esta en la posicion numero %d del arreglo", numeroBusqueda, middle + 1);
-                return 1;
+                max = middle - 1;
             }
             else
             {
-                if (numeroBusqueda < arreglo[middle])
-                {
-                    max = middle - 1;
-                }
-                else
-                {
-                    min = middle + 1;
-                }
+                min = middle + 1;
             }
         }
-        printf("El numero %d no se encuentra el la ultimo arreglo registrado", numeroBusqueda);
-        return 0;
     }
+    printf("El numero %d no se encuentra el la ultimo arreglo registrado", numeroBusqueda);
+    return 0;
+}
 
-    void main()
+void main()
+{
+    // Var for ask
+    int opcionUsuario, numeroBusqueda;
+    // Var for Random array
+    int range, caract;
+    int *cadenaNum;
+    do
     {
-        // Var for ask
-        int opcionUsuario, numeroBusqueda;
-        // Var for Random array
-        int range, caract;
-        int *cadenaNum;
-        do
+        printf("\n\n");
+        printf("-----------------------------------------");
+        puts("\nSeleccione una opcion");
+        puts("1.- Generar datos");
+        puts("2.- Imprimir la ultima cadena registrada");
+        puts("3.- Buscar numero con Secuencial");
+        puts("4.- Buscar numero con Binaria");
+        puts("5.- Salir del programa");
+        fflush(stdin);
+        scanf("%d", &opcionUsuario);
+        switch (opcionUsuario)
         {
-            printf("\n\n");
-            printf("-----------------------------------------");
-            puts("\nSeleccione una opcion");
-            puts("1.- Generar datos");
-            puts("2.- Imprimir la ultima cadena registrada");
-            puts("3.- Buscar numero con Secuencial");
-            puts("4.- Buscar numero con Binaria");
-            puts("5.- Salir del programa");
+        case 1:
+            // Random Numbers
+            // Clear memory
+            free(cadenaNum);
+            puts("Porfavor digite cuantos digitos habra en la cadena");
+            scanf("%d", &caract);
             fflush(stdin);
-            scanf("%d", &opcionUsuario);
-            switch (opcionUsuario)
+            puts("Porfavor digite el limite del rango para obtener los numeros aleatorios");
+            scanf("%d", &range);
+            fflush(stdin);
+            // Random Seed
+            srand(time(NULL));
+            cadenaNum = malloc(caract * sizeof(int));
+            // Save numbers
+            for (int i = 0; i <= caract; i++)
             {
-            case 1:
-                // Random Numbers
-                // Clear memory
-                free(cadenaNum);
-                puts("Porfavor digite cuantos digitos habra en la cadena");
-                scanf("%d", &caract);
-                fflush(stdin);
-                puts("Porfavor digite el limite del rango para obtener los numeros aleatorios");
-                scanf("%d", &range);
-                fflush(stdin);
-                // Random Seed
-                srand(time(NULL));
-                cadenaNum = malloc(caract * sizeof(int));
-                // Save numbers
-                for (int i = 0; i <= caract; i++)
-                {
-                    cadenaNum[i] = rand() % range + 1;
-                }
-                ordenarDatos(cadenaNum, caract);
-                // Print array
-                printf("Cadena Generada con exito");
-                break;
-            case 2:
-                printf("El ultimo arreglo en memoria es:\n");
-                imprimirDatos(cadenaNum, caract);
-                break;
-            case 3:
-                puts("Porfavor digite el numero que desea buscar");
-                scanf("%d", &numeroBusqueda);
-                fflush(stdin);
-                busquedaSecuencial(cadenaNum, caract, numeroBusqueda);
-                break;
-            case 4:
-                puts("Porfavor digite el numero a buscar");
-                scanf("%d", &numeroBusqueda);
-                fflush(stdin);
-                busquedaBinaria(cadenaNum, caract, numeroBusqueda);
-                break;
-            case 5:
-                printf("Saliendo del programa");
-                exit(0);
-                break;
-            default:
-                printf("Porfavor digite una opción valida");
-                break;
+                cadenaNum[i] = rand() % range + 1;
             }
+            ordenarDatos(cadenaNum, caract);
+            // Print array
+            printf("Cadena Generada con exito");
+            break;
+        case 2:
+            printf("El ultimo arreglo en memoria es:\n");
+            imprimirDatos(cadenaNum, caract);
+            break;
+        case 3:
+            puts("Porfavor digite el numero que desea buscar");
+            scanf("%d", &numeroBusqueda);
+            fflush(stdin);
+            busquedaSecuencial(cadenaNum, caract, numeroBusqueda);
+            break;
+        case 4:
+            puts("Porfavor digite el numero a buscar");
+            scanf("%d", &numeroBusqueda);
+            fflush(stdin);
+            busquedaBinaria(cadenaNum, caract, numeroBusqueda);
+            break;
+        case 5:
+            printf("Saliendo del programa");
+            exit(0);
+            break;
+        default:
+            printf("Porfavor digite una opción valida");
+            break;
+        }
 
-        } while (opcionUsuario != 5);
-    }
+    } while (opcionUsuario != 5);
+}

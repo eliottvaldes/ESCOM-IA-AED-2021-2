4a5dd75e44a8d49947ea9c0581c7570af34329fc
Eliot Valdes <eliottvaldes@hotmail.com>
2022-03-10 04:36:44 -0600

    imp(e) - funcionalidad des estructura anidada

--- ELIOT/estructuras.c
+++ ELIOT/estructuras.c
@@ -13,6 +13,25 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+// creamos nuestras estructuras
+typedef struct infoDireccion
+{
+    char calle[40];
+    char numero[5];
+    char estado[40];
+    char codigoPostal[6];
+
+} Direccion;
+
+// estructura general
+typedef struct infousuario
+{
+    char nombre[40];
+    char apellido[40];
+    char telefono[15];
+    Direccion dir;
+} Usuario;
+
 // print animation
 int sleep();
 void loading()
@@ -29,17 +48,56 @@ void loading()
 }
 
 // CREAMOS PROTOTIPOS DE FUNCIONES
-void registrarEstructura();
-void mostrarEstructura();
-void buscarEnEstructura();
+// void registrarUsuario(Usuario *usr);
+// void mostrarUsuario(Usuario usr);
 
+void registrarUsuario(Usuario *usr, int contadorDeEstructuras)
+{
+    // Obtenemos los datos del usuario
+    puts("\tIngresa los datos del usuario =>");
+    puts("\n---------------------------------------------\n");
+    puts("Nombre: ");
+    fgets(usr->nombre, sizeof(usr->nombre), stdin);
+    puts("Apellido: ");
+    fgets(usr->apellido, sizeof(usr->apellido), stdin);
+    puts("Telefono: ");
+    fgets(usr->telefono, sizeof(usr->telefono), stdin);
+    puts("Direccion calle: ");
+    fgets(usr->dir.calle, sizeof(usr->dir.calle), stdin);
+    puts("Direccion numero: ");
+    fgets(usr->dir.numero, sizeof(usr->dir.numero), stdin);
+    puts("Direccion estado: ");
+    fgets(usr->dir.estado, sizeof(usr->dir.estado), stdin);
+    puts("Direccion codigo postal: ");
+    fgets(usr->dir.codigoPostal, sizeof(usr->dir.codigoPostal), stdin);
+    puts("\n---------------------------------------------\n");    
+};
+
+void mostrarUsuario(Usuario usr, int contadorDeEstructuras)
+{
+    for (int i = 0; i < contadorDeEstructuras; i++)
+    {
+        printf("\nNombre: \t %s", usr.nombre);
+        printf("\nApellido: \t %s", usr.apellido);
+        printf("\nTelefono: \t %s", usr.telefono);
+        printf("\nDireccion calle: \t %s", usr.dir.calle);
+        printf("\nDireccion numero: \t %s", usr.dir.numero);
+        printf("\nDireccion estado: \t %s", usr.dir.estado);
+        printf("\nDireccion codigo postal: \t %s", usr.dir.codigoPostal);
+        puts("\n---------------------------------------------\n");
+    }
+}
 
 // main function
 void main()
 {
+
+    Usuario *usr = (Usuario*)malloc(50 * sizeof(Usuario));
+
     // variables de menu
-    int opcionSalir = 4;
+    int opcionSalir = 3;
     int opcionSeleccionMenu;
+    int contadorDeEstructuras = 0;
 
     // print greetings
     puts("**************************************");
@@ -54,8 +112,7 @@ void main()
         puts("Digita la opcion que deseas realizar:");
         puts("1.- Registrar datos en estructura");
         puts("2.- Mostrar datos de estructura");
-        puts("3.- Buscar datos en estructura");
-        puts("4.- Salir");
+        puts("3.- Salir");
         puts("--------------------------------------");
         scanf("%d", &opcionSeleccionMenu);
         fflush(stdin);
@@ -68,6 +125,9 @@ void main()
             puts("\n\t *** Has seleccionado la opcion para registrar datos en estructura ***\n ");
             // Start Coding
 
+            registrarUsuario(usr, contadorDeEstructuras);
+            contadorDeEstructuras++;
+            // printf("\n\n\tHay %d Estructuras \n\n", contadorDeEstructuras);
             // End of Code
 
             break;
@@ -76,21 +136,12 @@ void main()
 
             puts("\n\t *** Has seleccionado la opcion para mostrar los datos de la estructura ***\n ");
             // Start Coding
-
+            mostrarUsuario(*usr, contadorDeEstructuras);
             // End of Code
 
             break;
 
         case 3:
-
-            puts("\n\t *** Has seleccionado la opcion de buscar datos en la estructura ***\n ");
-            // Start Coding
-
-            // End of Code
-
-            break;
-
-        case 4:
             printf("\nSALIENDO DEL PROGRAMA ");
             loading();
             puts("\n\n *** ADIOS *** :)\n ");
@@ -102,4 +153,87 @@ void main()
             break;
         }
     } while (opcionSeleccionMenu != opcionSalir);
+
+    free(usr);
 }
+
+// #include <stdio.h>
+
+// typedef struct
+// {
+//     char *firstName;
+//     char *lastName;
+//     int rollNumber;
+
+// } STUDENT;
+
+// typedef struct
+// {
+//     char *calle[40];
+//     char *numero[5];
+//     char *estado[40];
+//     char *codigoPostal[6];
+
+// } DIRECCION;
+
+// // estructura genera
+// typedef struct
+// {
+//     char *nombre[40];
+//     char *apellido[40];
+//     char *telefono[15];
+//     DIRECCION *direccion;
+// } USUARIO;
+
+// int registrarUsuario(USUARIO *usr, int contadorDeEstructuras)
+// {
+//     puts("\tIngresa los datos del usuario =>");
+//     puts("\n---------------------------------------------\n");
+//     puts("Nombre: ");
+//     scanf("%s", usr[contadorDeEstructuras].nombre);
+//     puts("Apellido: ");
+//     scanf("%s", usr[contadorDeEstructuras].apellido);
+//     puts("Telefono: ");
+//     scanf("%s", usr[contadorDeEstructuras].telefono);
+//     puts("Direccion calle: ");
+//     scanf("%s", usr[contadorDeEstructuras].direccion->calle);
+//     puts("Direccion numero: ");
+//     scanf("%s", usr[contadorDeEstructuras].direccion->numero);
+//     puts("Direccion estado: ");
+//     scanf("%s", usr[contadorDeEstructuras].direccion->estado);
+//     puts("Direccion codigo postal: ");
+//     scanf("%s", usr[contadorDeEstructuras].direccion->codigoPostal);
+//     puts("\n---------------------------------------------\n");
+
+//     return contadorDeEstructuras++;
+// };
+
+// mostrarDatos(USUARIO *usr, int contadorDeEstructuras)
+// {
+//     for (int i = 0; i < contadorDeEstructuras; i++)
+//     {    
+
+//         printf("\nNombre: \t %s", usr[contadorDeEstructuras].nombre);
+//         printf("\nApellido: \t %s", usr[contadorDeEstructuras].apellido);
+//         printf("\nTelefono: \t %s", usr[contadorDeEstructuras].telefono);
+//         printf("\nDireccion calle: \t %s", usr[contadorDeEstructuras].direccion->calle);
+//         printf("\nDireccion numero: \t %s", usr[contadorDeEstructuras].direccion->numero);
+//         printf("\nDireccion estado: \t %s", usr[contadorDeEstructuras].direccion->estado);
+//         printf("\nDireccion codigo postal: \t %s", usr[contadorDeEstructuras].direccion->codigoPostal);
+//         puts("\n---------------------------------------------\n");
+//     }
+// }
+// void main()
+// {
+
+
+//     // int cantidadEstructuras = 50;
+//     int contadorDeEstructuras = 1;
+//     int y;
+//     USUARIO *usr = malloc(50 * (sizeof *usr));
+
+//     contadorDeEstructuras = registrarUsuario(usr, contadorDeEstructuras);
+    
+//     mostrarDatos(usr, contadorDeEstructuras);
+    
+// }

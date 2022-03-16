bbc71a123adfd40cd0adbad4b03633475326ce71
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-03-16 17:16:54 -0600

    add-iostream and namespace std

--- ALVARO/Vector/vectores.cpp
+++ ALVARO/Vector/vectores.cpp
@@ -1,10 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<iostream>
 #include<vector>
-
-
+#include<stack>
+//vector y stack
+using namespace std;
 
 int main(){
+stack<int> objeto;
+for (int i = 0; i < 50; i++)
+{
+    objeto.push(rand());
+}
+for (int i = 0; i < 50; i++)
+{
+    printf("%i",objeto);
+}
+
 
 return 0;  
 }

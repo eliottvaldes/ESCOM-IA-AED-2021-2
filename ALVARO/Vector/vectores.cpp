434c7182f8290641a45ea717b5e882f0eed21c6e
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-03-16 18:15:35 -0600

    add-stack program

--- ALVARO/Vector/vectores.cpp
+++ ALVARO/Vector/vectores.cpp
@@ -8,15 +8,22 @@ using namespace std;
 
 int main(){
 stack<int> objeto;
+stack<int>aux;
 for (int i = 0; i < 50; i++)
 {
-    objeto.push(rand());
+    objeto.push(rand()%10);
+    int n=objeto.top();
+    cout<<n;cout<<" ";
 }
+cout<<endl;
 for (int i = 0; i < 50; i++)
 {
-    printf("%i",objeto);
+    aux.push(objeto.top());
+    objeto.pop();
+    cout<<aux.top()<<" ";
+
 }
 
-
+system("pause");
 return 0;  
 }

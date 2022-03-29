5cb0c2f3a58593ed491e2dcafcad38cc58dba5cf
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-03-28 19:09:11 -0600

    mod-Programa completo de cola menu e inicio de lista

---
+++ ALVARO/Structs/Lista.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdlib.h>
+
+typedef struct{
+        int dato;
+        Nodo *sig;
+}Nodo;
+
+void nuevo_nodo(Nodo *&lista,int num);
+
+int main(){
+    Nodo * inicio=NULL;
+    int cond=0;
+    do
+    {
+        if (inicio==NULL)
+        {
+            //nuevo nodo
+            //enlace sig nuevo
+            //nodo a null
+            //poner datos
+            //actualiza inicio
+            
+        }
+
+
+    } while (cond==0);
+    
+
+
+    return 0;
+}
+//crea nodo nuevo
+//poner datos
+//Enlace a siguiente null

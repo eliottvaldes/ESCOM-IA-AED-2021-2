66e002fbc14e1b8a5b327f0205d1af1e92b0e7aa
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-04-11 17:24:15 -0500

    mod-se modifica para solo usar priority queue

--- ALVARO/Structs/Cola_prioridad.cpp
+++ ALVARO/Structs/Cola_prioridad.cpp
@@ -6,6 +6,7 @@
  */
 #include<iostream>
 #include<stdlib.h>
+#include<queue>
 using namespace std;
 
 
@@ -18,16 +19,13 @@ typedef struct Nodo{
 }Nodo;
 
 //Prototipos de funciones
-bool ins_elem(Nodo **list,int dato);
-bool elim_elem(Nodo **list,int tam);
-int size_que(Nodo *list);
-bool most_que(Nodo *list);
+bool ins_elem();
+bool elim_elem();
+
+bool most_que();
 
 int main(){
     int opt,tam=0,dato;
-    Nodo *inicio;
-    inicio=new Nodo();
-    inicio->sig=NULL;
     do
     {   
         cout<<"1.-Insertar elemento"<<endl<<"2.-Eliminar elemento"<<endl<<"3.-Ver elementos"<<endl;
@@ -37,15 +35,14 @@ int main(){
         case 1:
             cout<<"Inserta un numero"<<endl;
             cin>>dato;
-            ins_elem(&inicio,dato);
+            
             
             break;
         case 2:
-            tam=size_que(inicio);
-            elim_elem(&inicio,tam);
+            
             break;
         case 3:
-            most_que(inicio);
+           
             break;
         default:
             cout<<"Introduce una opcion valida";
@@ -56,76 +53,20 @@ int main(){
 
 }
 
-int size_que(Nodo *list){
-    Nodo *iterador;
-    int a;
-    iterador=list->sig;
-    while (iterador->sig!=NULL)
-    {
-        iterador=iterador->sig;
-        a++;
-    }
-    
-    return a;
-}
 
-bool ins_elem(Nodo **list,int dato){
-    Nodo *new_nod;
-    Nodo *iterador=(*list)->sig;
-    Nodo *antes;
-    
-    new_nod=new Nodo();
-    new_nod->sig=NULL;
-    if ((*list)->sig==NULL)
-    {
-        (*list)->sig=new_nod;
-        cout<<"Primer Numero insertado exitosamente"<<endl;
-        return true;
-    }
-    while ((iterador->sig!=NULL) && (iterador->dato<dato))
-    {
-        antes=iterador;
-        iterador=iterador->sig;
-    }
-    system("pause");
-    antes->sig=new_nod;//aqui se muere el programa
-    new_nod->sig=iterador;
-    return true;
+
+bool ins_elem(){
+   
 }
 
 
-bool elim_elem(Nodo **list,int tam){
-    Nodo *iterator;
-    Nodo *antes;
-    iterator=(*list)->sig;
-    while (iterator->sig!=NULL)
-    {   
-        antes=iterator;
-        iterator=iterator->sig;
-    }
-    antes->sig=NULL;
-    delete iterator;
-    return true;
+bool elim_elem(){
+   
 }
 
 
-bool most_que(Nodo *list){
-    Nodo *iterador;
-    iterador=list->sig;
-    if (iterador==NULL)
-    {
-        cout<<"No hay elementos"<<endl;
-        return false;
-
-    }
-    
-    while (iterador->sig!=NULL)
-    {
-        cout<<iterador->dato<<"-->";
-        iterador=iterador->sig;
-    }
-    cout<<endl;
-    return true;
+bool most_que(){
+   
 }
 
 /*

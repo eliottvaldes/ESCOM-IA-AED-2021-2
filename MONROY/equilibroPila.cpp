8890e7e6757b27bae5bbaef1943a06ff796da126
Jose <mnry7999@gmail.com>
2022-04-27 13:59:02 -0500

    Se agrega el menú

--- MONROY/equilibroPila.cpp
+++ MONROY/equilibroPila.cpp
@@ -6,8 +6,30 @@
 
 #include <iostream>
 #include <stack>
+using namespace std;
 
 int main()
 {
-    
+    stack<char> contenedor;
+    int opcionUsuario;
+    do
+    {
+        cout << "Elija una opcion" << endl;
+        cout << "1 .- Ingresar expresion" << endl;
+        cout << "2 .- Salir del programa" << endl;
+        cin >> opcionUsuario;
+        switch (opcionUsuario)
+        {
+        case 1:
+            break;
+        case 2:
+            cout << "Saliendo del programa" << endl;
+            return(0);
+            break;
+        default:
+            cout << "Elija una opcion valida" << endl;
+            break;
+        }
+    } while (opcionUsuario != 2);
+    return(0);
 }
